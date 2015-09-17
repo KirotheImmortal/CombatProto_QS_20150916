9fb3cb3b2d94421ae472f002dd6b5ac79f630845
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-09-17 18:07:30 -0500

    Added Bullet and Gravity Scripts
    Intened to make bullet attacks

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    
+
+    public float speed;
+
+    public bool live;
+
+    
+    void Start()
+    {
+       
+
+        StartCoroutine(UpdatePos());
+    }
+
+    IEnumerator UpdatePos()
+    {
+        while (live)
+        {
+            transform.position += transform.forward * speed;
+            yield return null;
+        }
+
+        print("Detonated");
+
+        Destroy(gameObject);
+
+    }
+
+}
