77ea5654e447a7182269b0bb6333b7ffcdfc8b09
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-09-17 16:57:33 -0500

    More On the FSM and StateManager
    Created a StateManager to manage a FSM created within it

--- Assets/scripts/QStateMachine.cs
+++ Assets/scripts/QStateMachine.cs
@@ -3,6 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 
 
+// Programmer: Quinton "Kiro" Baudoin
+// Purpose: Modular FSM
+// Use summary:
+//     QState: Create and manage each State.
+//          Each state is given a name of the state and a state it can fallow.
+//    QStateMachine:
+//          Premade State Machine.
+//          You may make your own variant as your program needs.
+//          Uses a list to store all the states.
+//          Using Getter and Setters to change and run current state based on sname and sprev strings in the QStates
+
+
 // Prefixes:
 // Q     :     Quinton
 // d    :     Delegate
@@ -15,9 +27,9 @@ using System.Collections.Generic;
 // 
 // Suffixes:
 
-    /// <summary>
-    /// State Object
-    /// </summary>
+/// <summary>
+/// State Object
+/// </summary>
 public class QStates
 {
     public QStates(string name, string prev)
@@ -64,6 +76,7 @@ public class QStateMachine
     /// <summary>
     /// State currently in.
     /// When Modified the state will play its delegates if delegates have functions stored
+    /// NOTE: Void functions then IEnumerable functions
     /// </summary>
     QStates current
     {
@@ -73,15 +86,18 @@ public class QStateMachine
         {
             cur = value;
             if (current.dv_stateFunctions != null)
-            {
                 current.dv_stateFunctions();
-            }
+
             if (current.die_stateFunctions != null)
-            {
                 current.die_stateFunctions();
-            }
+
         }
     }
+
+    public QStates currentState
+        {
+        get { return cur; }
+        }
     /// <summary>
     /// State intended to change to. If allowed the current state will be changed to the value the nextState was changed to.
     /// </summary>
@@ -94,7 +110,7 @@ public class QStateMachine
             nex = value;
             if (current == null)
                 current = value;
-            else if (current.sname == current.sprev || nextState.sprev == null)
+            else if (current.sname == nex.sprev || nextState.sprev == null)
             {
                 current = value;
             }
@@ -103,10 +119,19 @@ public class QStateMachine
 
     }
 
+    /// <summary>
+    /// Creates a QState based on the params passed in.
+    /// </summary>
+    /// <param name="name"> Name of QState</param>
+    /// <param name="prevname">Name of QState to fallow</param>
     public void addState(string name, string prevname)
     {
         ls_States.Add(new QStates(name, prevname));
     }
+    /// <summary>
+    /// Removes a state from the list based on name variable
+    /// </summary>
+    /// <param name="name"> Name assigned to QState</param>
     public void removeState(string name)
     {
         foreach (QStates s in ls_States)
@@ -115,6 +140,12 @@ public class QStateMachine
                 ls_States.Remove(s);
         }
     }
+    /// <summary>
+    /// Changes the nextState variable.
+    /// To add further detail..
+    /// If the state that is passed in to fallow the current state it is then added ass current state.
+    /// </summary>
+    /// <param name="name"></param>
     public void changeState(string name)
     {
         foreach (QStates s in ls_States)
@@ -124,10 +155,18 @@ public class QStateMachine
                 return;
             }
     }
+    /// <summary>
+    /// Clears the State list
+    /// </summary>
     public void clearStateList()
     {
         ls_States.Clear();
     }
+    /// <summary>
+    /// Gtets the state using name variable.
+    /// </summary>
+    /// <param name="name">Would be the sname in QStates</param>
+    /// <returns></returns>
     public QStates getState(string name)
     {
         foreach (QStates s in ls_States)
@@ -135,11 +174,12 @@ public class QStateMachine
                 return s;
 
         return null;
-        
+
     }
+    //public addVoid()
 
 
-    
+
 
 }
 
