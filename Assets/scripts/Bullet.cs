e978ddcdc5c7102ff9f02a3181f8dfd10400c678
Quinton Baudoin <quinton.baudoin@LAF-H2-7D741E.lafa.ad.aie.edu>
2015-09-18 14:09:12 -0500

    did more to bullets.
    NOTE: Gravity does not work properly do not use.

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -6,15 +6,37 @@ public class Bullet : MonoBehaviour
     
 
     public float speed;
+    public float MaxDistance;
+    public float Damage;
+
+    Vector3 startpos;
 
     public bool live;
+    
 
     
     void Start()
     {
-       
+        updateLocation();
 
         StartCoroutine(UpdatePos());
+    }
+
+    public void updateLocation()
+    {
+       startpos = transform.position;
+    }
+
+    public float Hit()
+    {
+        live = false;
+        return Damage;
+    }
+
+
+    void Expload()
+    {
+        Destroy(gameObject);
     }
 
     IEnumerator UpdatePos()
@@ -22,12 +44,16 @@ public class Bullet : MonoBehaviour
         while (live)
         {
             transform.position += transform.forward * speed;
+
+            if (Vector3.Distance(startpos, transform.position) > MaxDistance)
+                live = false;
+
             yield return null;
         }
 
         print("Detonated");
 
-        Destroy(gameObject);
+        Expload();
 
     }
 
--- Assets/scripts/Gravity.cs
+++ Assets/scripts/Gravity.cs
@@ -4,17 +4,33 @@ using System.Collections;
 public class Gravity : MonoBehaviour
 {
 
-   public float gpull;
+    public float gpull;
+   // float angleOfEntry;
 
     void OnTriggerStay(Collider c)
     {
         if (c.gameObject.GetComponent<Bullet>())
         {
-           Vector3 bullet = c.gameObject.GetComponent<Transform>().forward;
-           float distance = Vector3.Distance(transform.position, bullet);
+           Vector3 center = transform.position;
+           Vector3 bullet = c.gameObject.GetComponent<Transform>().forward - center;
+            
+           float dis = Vector3.Distance(center, bullet);
 
-            c.gameObject.GetComponent<Transform>().forward /= distance;
-            print("somethinghappend");
+           
+
+            // // c.gameObject.GetComponent<Transform>().forward -= new Vector3((distance * gpull)/100, 0 , (distance * gpull)/100);
+            // // print("somethinghappend");
+
+            // float dis = Vector2.Distance(new Vector2(bullet.x,bullet.z), new Vector2(transform.position.x, transform.position.z));
+             float Hyp = Mathf.Sqrt((dis * dis) + (dis * dis));
+            float angleOfEntry = Mathf.Sin(Hyp/dis);
+
+            
+
+            bullet.x = (center.x + dis * Mathf.Sin(angleOfEntry));
+            bullet.z = (center.z + dis * Mathf.Cos(angleOfEntry));
+            //c.gameObject.GetComponent<Transform>().position = center + new Vector3(bullet.x  + (center.x + gpull), 0, bullet.z + (center.z + gpull));
+            //c.gameObject.GetComponent<Transform>().forward += new Vector3(bullet.x, 0, bullet.z);
 
         }
 
